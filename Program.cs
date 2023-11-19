a3b82ff2175b9b9ada94e99362002bc68b761a6c
GabrielGNM <gabriel.gnm@gmail.com>
2023-11-19 12:01:37 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// PresentationLayer.cs
+class Program
+{
+    static void Main()
+    {
+        DataLayer dataLayer = new DataLayer();
+        BusinessLogicLayer businessLogicLayer = new BusinessLogicLayer();
+
+        // Extração de dados
+        List<string> rawData = dataLayer.ExtractData();
+
+        // Transformação de dados
+        List<string> transformedData = businessLogicLayer.TransformData(rawData);
+
+        // Carregamento de dados transformados
+        dataLayer.LoadData(transformedData);
+
+        Console.ReadLine();
+    }
+}
